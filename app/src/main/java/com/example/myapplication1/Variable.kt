4ed6d00c2f47728437c17de6a511d307a221af65
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2024-10-11 19:56:37 +0700

    kelas

---
+++ app/src/main/java/com/example/myapplication1/Variable.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication1
+
+
+
+fun main(){
+    // Byte
+    val ByteValue: Byte = 10
+    println("Byte: $ByteValue")
+
+    // Int
+    val intValue: Int = 30
+    println(intValue)
+
+    // Float
+    val floatValue: Float = 50.6f
+    println(floatValue)
+
+    // Double
+    val doubleValue: Double = 60.6
+    println(doubleValue)
+
+    // Char
+    val charValue: Char = 'A'
+    println(charValue)
+
+    // Boolean
+    val booleanValue: Boolean = true
+    println(booleanValue)
+
+    // String
+    var stringValue: String = "Hello, World!"
+    println(stringValue)
+
+    stringValue = "Hello, Kotlin!"
+    println("String: $stringValue")
+}
