308457feec282ad7ae6ceee89390ce1c78b39339
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2024-10-13 18:58:11 +0700

    ConditionalExpression  pertama

---
+++ app/src/main/java/com/example/myapplication1/ConditionalExpression.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication1
+
+fun ConditionalStatement(){
+    println("=== Conditional Expression ===")
+
+
+    // If-Else
+    val number = 10
+    if (number > 0){
+        println("Positive number")
+    } else {
+        println("Negative number")
+    }
