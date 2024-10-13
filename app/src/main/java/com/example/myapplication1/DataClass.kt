59b61a65a3a1ec642a549288031379eec69299a3
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2024-10-13 19:13:27 +0700

    DataClass finish

--- app/src/main/java/com/example/myapplication1/DataClass.kt
+++ app/src/main/java/com/example/myapplication1/DataClass.kt
@@ -10,3 +10,22 @@ data class DataClass(
     val id: Int,
     var email: String
 )
+fun main(){
+    val data = DataClass(1,"indra@gmail.com")
+    println(data)
+
+
+    // Fungsi equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data==data2) // false
+
+
+    // Fungsi Copy
+    val data3 = data.copy()
+    println(data3)
+
+
+    // Fungsi Copy dan perubahan
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+}
