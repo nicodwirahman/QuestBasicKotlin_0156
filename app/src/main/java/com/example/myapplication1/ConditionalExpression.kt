f321ce80c7f3af88fdd2e3cf1de27335a1229add
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2024-10-13 18:59:02 +0700

    ConditionalExpression finish

--- app/src/main/java/com/example/myapplication1/ConditionalExpression.kt
+++ app/src/main/java/com/example/myapplication1/ConditionalExpression.kt
@@ -33,3 +33,16 @@ fun ConditionalStatement(){
         println("Invalid Number")
     }
 }
+fun Perulangan(){
+    println()
+    println("=== Perulangan ===")
+
+
+    for (i in 1..5){
+        println("Perulangan ke $i")
+    }
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars){
+        println("Car type: $car")
+    }
+}
