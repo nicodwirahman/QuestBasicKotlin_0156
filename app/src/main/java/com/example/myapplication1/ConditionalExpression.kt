892cd8842cdc5dd216b9b66961deb3f3c1909362
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2024-10-13 18:58:39 +0700

    ConditionalExpression kedua

--- app/src/main/java/com/example/myapplication1/ConditionalExpression.kt
+++ app/src/main/java/com/example/myapplication1/ConditionalExpression.kt
@@ -11,3 +11,25 @@ fun ConditionalStatement(){
     } else {
         println("Negative number")
     }
+    val day = 1
+    when (day){
+        1 -> println ("Minggu")
+        2 -> println ("Senin")
+        3 -> println ("Selasa")
+        4 -> println ("Rabu")
+        5 -> println ("Kamis")
+        6 -> println ("Jumat")
+        7 -> println ("Sabtu")
+        else -> println ("Invalid")
+    }
+
+
+    // Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+}
