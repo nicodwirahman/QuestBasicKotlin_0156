298e32fb543c135251f498a971fdb50cd9994696
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2024-10-13 22:43:14 +0700

    di hapus kerena di buat doble fun contoh set ya

--- app/src/main/java/com/example/myapplication1/Collections.kt
+++ app/src/main/java/com/example/myapplication1/Collections.kt
@@ -55,27 +55,4 @@ fun ContohSet(){
     val shapeLocked: Set<String> = mutableShapes
     println(shapeLocked)
 }
-fun ContohSet(){
-    println()
-    println("=== Set ===")
 
-
-    // Set Read-Only
-    val readOnlyAbjad = setOf("A", "B", "C")
-    println(readOnlyAbjad)
-
-
-    // Set Mutable
-    val mutableShapes: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
-    println(mutableShapes)
-
-
-    // Menambahkan data kedalam Set Mutable
-    mutableShapes.add("Rectangle")
-    println(mutableShapes)
-
-
-    // Set Read-Only
-    val shapeLocked: Set<String> = mutableShapes
-    println(shapeLocked)
-}
